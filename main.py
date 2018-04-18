6f0063d264646641dfd4b5532cd46e24a27d43d4
AmeyKamat <ask5332@gmail.com>
2018-04-18 23:07:08 +0530

    updating requirements.txt

--- main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from speechRecognition.speech_recognizer import SpeechRecognizer
-
-speechRecognizer = SpeechRecognizer(['GoogleSpeechRecognitionEngine', 'SphinxSpeechRecognitionEngine']);
-print(speechRecognizer.listen());
